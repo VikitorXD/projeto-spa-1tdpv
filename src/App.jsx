1de4596220566c44a8bb381b1a079943abeb6918
VikitorXD <rm551886@fiap.com.br>
2023-08-29 22:18:57 -0300

    ....

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import reactLogo from "./assets/react.svg";
 import Cabecalho from "./components/Cabecalho";
+import Conteudo from "./components/Conteudo";
 
 export default function App() {
   
@@ -17,15 +18,8 @@ export default function App() {
 
 
         {/* Uma seção com uma div com 3 parágrafos, com 3 linhas de lorem, uma imagem */}
-        <section>
-          <div>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit accusantium repudiandae enim delectus deserunt itaque necessitatibus, obcaecati fugiat suscipit quasi error animi impedit esse distinctio! Cum impedit modi tempora laborum?</p>
-            <p>Incidunt quaerat id neque non dolores atque architecto, praesentium rerum, veritatis magnam iure rem commodi itaque illo vel modi numquam libero quam labore exercitationem laboriosam fuga est. Nam, nostrum commodi!</p>
-            <p>Velit tempora, hic facilis suscipit, beatae aliquid saepe harum aspernatur deserunt unde dicta blanditiis quod maiores accusantium sunt, itaque odit. Facilis magnam eius sapiente illo reiciendis quisquam saepe nisi at?</p>
-            <img src={reactLogo} alt={reactLogoTextoAlt} />
-          </div>
-        </section>
 
+      <Conteudo reactLogoProps={reactLogo} reactLogoTextoAlt={reactLogoTextoAlt}/>
         
 
         {/* Um rodapé com uma div, uma lista ul com 3 itens e links para redes sociais */}
