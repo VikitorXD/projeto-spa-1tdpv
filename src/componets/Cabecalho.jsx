841bd3dd610dd774789d7ac40188a2b3c4cc1101
VikitorXD <rm551886@fiap.com.br>
2023-09-14 21:40:41 -0300

    alterando codigo

--- src/componets/Cabecalho.jsx
+++ src/componets/Cabecalho.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 export default function Cabecalho() {
   return (
     <>
@@ -5,13 +7,13 @@ export default function Cabecalho() {
         <h1>Vite + React / Coded By- PF0670</h1>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link to="/">HOME</Link>
           </li>
           <li>
-          <Link to="/produtos">Produtos</Link>
+            <Link to="/produtos">PRODUTOS</Link>
           </li>
           <li>
-          <Link to="/editar/produtos/1">Produto-1</Link>
+          <Link to="/editar/produtos/1">PRODUTO-1</Link>
           </li>
         </ul>
       </header>
